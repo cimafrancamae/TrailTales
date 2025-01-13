822873684236194c5f960324e4d64bb0974121b7
maeci <cimafrancamae@gmail.com>
2025-01-13 17:59:47 +0800

    fix: page redirection after logout

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { View, Button, Text } from 'react-native';
-import Login from '@/components/auth/login';
-import Register from '@/components/auth/register';
+import Login from '@/app/auth/login';
+import Register from '@/app/auth/register';
 import { useAuth } from '@/hooks/useAuth';
-import { useRouter } from 'expo-router';
+import { useRouter, useSegments } from 'expo-router';
 
 const Index = () => {
-  const [isRegistering, setIsRegistering] = useState(false); // Toggle between Login and Register forms
+  const [isRegistering, setIsRegistering] = useState(false); 
   const { user, loading } = useAuth();
   const router  = useRouter();
 
   useEffect(() => {
-    console.log("User:", user, "Loading:", loading);
     if (user && !loading) {
       router.replace('/(tabs)');
     }
   }, [user, loading]);
+  
 
   if (loading) {
     return (
@@ -27,11 +27,16 @@ const Index = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {isRegistering ? <Register /> : <Login />}
-      <Button
-        title={isRegistering ? 'Already have an account? Login' : "Don't have an account? Register"}
-        onPress={() => setIsRegistering(!isRegistering)}
-      />
+      {!loading && !user && (
+        <>
+          {isRegistering ? <Register /> : <Login />}
+          <Button
+            title={isRegistering ? 'Already have an account? Login' : "Don't have an account? Register"}
+            onPress={() => setIsRegistering(!isRegistering)}
+          />
+        </>
+      )}
+      {loading && <Text>Loading...</Text>}
     </View>
   );
 };
